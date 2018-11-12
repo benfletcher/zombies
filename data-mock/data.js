e62d0d150bda56b5c98cbdfa6f282b9cf767483d
bfletcher <bfletch@gmail.com>
2018-11-12 13:11:31 -0500

    Cleanup mock data. Prepare for mongo implementation.

--- data-mock/data.js
+++ data-mock/data.js
@@ -4,14 +4,17 @@ const data = {
   zombie: require('../data-mock/zombie.json'),
   location: require('../data-mock/location.json'),
 
+  zombieIds: () =>
+    new Set(data.zombie.map(({ id }) => Number(id))),
+
   locationNames: () =>
-    new Set(data.location.map(({ name }) => name.toLowerCase())),
+    new Map(data.location.map(({ name, id }) => [name.toLowerCase(), id])),
 
   locationIds: () =>
     new Set(data.location.map(({ id }) => Number(id))),
 
-  lastZombieId: 0,
-  lastLocationId: 0,
+  nextZombieId: Math.max(...data.zombieIds) + 1,
+  nextLocationId: Math.max(...data.locationIds) + 1,
 
   // TODO: internationalization
   // Will use ORM for sanitizing, this more a placeholder
@@ -24,7 +27,15 @@ const data = {
   isValidLocationId: id =>
     data.locationIds.has(id),
 
-  createZombie: ({ name, location, locationId }) => 0,
+  createZombie: ({ name, location, locationId }) => {
+    const newId = locationId ||
+
+      data.zombie.push({
+        id: data.nextZombieId,
+        name,
+        locationId:
+    })
+  },
 };
 
 module.exports = data;
--- routes/v1.js
+++ routes/v1.js
@@ -31,7 +31,7 @@ const v1 = data => {
     console.log(id);
     console.log(body);
     res.sendStatus(200);
-  })
+  });
 
   // Deletes
 
